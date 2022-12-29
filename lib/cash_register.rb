a7c5419939607f6251ea4a8e0cf687bc8ff64ae2
Michael <mikahado@gmail.com>
2022-12-29 14:13:03 -0700

    completes 2 out of 4 instance methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
 
+class CashRegister 
+
+    attr_accessor :discount, :total, :items, :last_transaction
+
+    def initialize(discount = 0)
+    
+        @discount = discount
+        @total = 0
+        @items = []
+     
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.last_transaction = price * quantity 
+        self.total += self.last_transaction
+        quantity.times do 
+            self.items << title
+        end
+    end
+
+
+
+end
