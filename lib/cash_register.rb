14c308dccd38785715a54d852ca437d0c6fb90e1
Michael <mikahado@gmail.com>
2022-12-29 18:48:05 -0700

    completes lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,13 +19,18 @@ class CashRegister
         end
     end
 
-    def apply_discount(title, discount)
+    def apply_discount
         if self.discount != 0
             discount_as_percent = (100.0 - self.discount.to_f) / 100
             self.total = (self.total * discount_as_percent).to_i
-            "After the discount, the total comes to #{self.total}"
+            "After the discount, the total comes to $#{self.total}."
         else
             "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total -= self.last_transaction
     end
 
 end
